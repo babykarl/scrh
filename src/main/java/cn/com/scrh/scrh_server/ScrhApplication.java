39a74bb969a07eebe4dcdaa7d33bcc38c3e3346a
babykarl <1834007615@qq.com>
2019-09-01 17:04:07 +0800

    启动类

---
+++ src/main/java/cn/com/scrh/scrh_server/ScrhApplication.java
@@ -0,0 +1,13 @@
+package cn.com.scrh.scrh_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrhApplication.class, args);
+	}
+
+}
